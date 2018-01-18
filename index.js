806b5c87a12b46ee7e2c1bb21f684701c755261a
sourabh sharma <mesourabhsharma@gmail.com>
2018-01-18 08:40:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('change:3 Welcome to sourabh World!.\nThis is a node.js microservice application running on Kubernetes cluster.\nThis cluster is created using jenking automation.\nName:Sourabh Sharma\nMid :M1035913');
+  res.send('change:3 Welcome to sourabh World!.\nThis is a node.js microservice application running on Kubernetes cluster.\nThis cluster is created using jenking automation.\nName:Sourabh Sharma\nMid :M1035913'');
 });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Sorbhs Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
+});
