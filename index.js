d0e3db60631e6c55eafe3b303ad3ea2caeabd87e
sourabh sharma <mesourabhsharma@gmail.com>
2018-01-21 20:14:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Welcome to Mindtree 7 World!.\nThis is a node.js microservice application running on Kubernetes cluster.\nThis cluster is created using jenking automation.\nName:Sourabh Sharma\nMid :M1035913');
+  res.send('Welcome to Mindtree 8 World!.\nThis is a node.js microservice application running on Kubernetes cluster.\nThis cluster is created using jenking automation.\nName:Sourabh Sharma\nMid :M1035913');
            });
 
 var server = app.listen(3000, function () {
